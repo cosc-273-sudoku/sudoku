0e0cdcf04a6e070d6f4a56ab4f4bafd1c833c67d
Alexander Lee <alexanderwlee@gmail.com>
2021-05-10 18:24:57 -0400

    change isEmpty to isFilled, and change formatting to 2 spaces..lol my b from last time

--- src/Board.java
+++ src/Board.java
@@ -1,86 +1,86 @@
-import java.util.Scanner;
 import java.io.*;
 import java.io.File;
+import java.util.Scanner;
 
 public class Board {
-	/*
-	 * creates a random sudoku board, creates a txt file, and writes the sudoku
-	 * board to that txt file if creating the file is successful
-	 */
-	public void generate(String filename) {
-		Sudoku sudoku = new Sudoku();
-		sudoku.fillValues();
-		this.createFile(sudoku, filename);
-	}
+  /*
+   * creates a random sudoku board, creates a txt file, and writes the sudoku
+   * board to that txt file if creating the file is successful
+   */
+  public void generate(String filename) {
+    Sudoku sudoku = new Sudoku();
+    sudoku.fillValues();
+    this.createFile(sudoku, filename);
+  }
 
-	/*
-	 * creates a txt file with the filename and writes the sudoku board into the txt
-	 * file with the filename upon successful creation
-	 */
-	public void createFile(Sudoku sudoku, String filename) {
-		try {
-			File myObj = new File(filename);
-			// checks to see if the file already exists
-			if (myObj.createNewFile()) {
-				System.out.println("File created: " + myObj.getPath());
-				this.exportToTxt(sudoku, filename);
-			} else {
-				System.out.println("The file already exists. I'll quit now");
-				System.exit(1);
-			}
-		}
-		// checks if an error occurred
-		catch (IOException e) {
-			System.out.println("Something went wrong. I'll quit now.");
-			System.exit(1);
-		}
-	}
+  /*
+   * creates a txt file with the filename and writes the sudoku board into the txt
+   * file with the filename upon successful creation
+   */
+  public void createFile(Sudoku sudoku, String filename) {
+    try {
+      File myObj = new File(filename);
+      // checks to see if the file already exists
+      if (myObj.createNewFile()) {
+        System.out.println("File created: " + myObj.getPath());
+        this.exportToTxt(sudoku, filename);
+      } else {
+        System.out.println("The file already exists. I'll quit now");
+        System.exit(1);
+      }
+    }
+    // checks if an error occurred
+    catch (IOException e) {
+      System.out.println("Something went wrong. I'll quit now.");
+      System.exit(1);
+    }
+  }
 
-	// writes the sudoku board into the txt file with the filename
-	public void exportToTxt(Sudoku sudoku, String filename) {
-		try {
-			PrintWriter pr = new PrintWriter(filename);
+  // writes the sudoku board into the txt file with the filename
+  public void exportToTxt(Sudoku sudoku, String filename) {
+    try {
+      PrintWriter pr = new PrintWriter(filename);
 
-			for (int i = 0; i < sudoku.matrix.length; i++) {
-				for (int j = 0; j < sudoku.matrix.length; j++) {
-					pr.print(sudoku.matrix[i][j] + " ");
-				}
-				pr.println();
-			}
-			pr.close();
-		}
-		// checks if the file does not exist
-		catch (FileNotFoundException e) {
-			System.out.println("File not found. I'll quit now.");
-			System.exit(1);
-		}
-	}
+      for (int i = 0; i < sudoku.matrix.length; i++) {
+        for (int j = 0; j < sudoku.matrix.length; j++) {
+          pr.print(sudoku.matrix[i][j] + " ");
+        }
+        pr.println();
+      }
+      pr.close();
+    }
+    // checks if the file does not exist
+    catch (FileNotFoundException e) {
+      System.out.println("File not found. I'll quit now.");
+      System.exit(1);
+    }
+  }
 
-	// reads from the txt file with the filename and returns a sudoku puzzle
-	public Sudoku readTxt(String filename) {
-		Sudoku sudoku = new Sudoku();
-		try {
-			Scanner scanner = new Scanner(new File(filename));
-			while (scanner.hasNextInt()) {
-				for (int i = 0; i < sudoku.matrix.length; i++) {
-					for (int j = 0; j < sudoku.matrix.length; j++) {
-						sudoku.matrix[i][j] = scanner.nextInt();
-					}
-				}
-			}
-			scanner.close();
-			System.out.println("Successful read from " + filename + ".");
-		}
-		// checks if file does not exist
-		catch (FileNotFoundException e) {
-			System.out.println("File not found. I'll quit now.");
-			System.exit(1);
-		}
-		// checks if an error occurred
-		catch (Exception e) {
-			System.out.println("Something went wrong. I'll quit now.");
-			System.exit(1);
-		}
-		return sudoku;
-	}
+  // reads from the txt file with the filename and returns a sudoku puzzle
+  public Sudoku readTxt(String filename) {
+    Sudoku sudoku = new Sudoku();
+    try {
+      Scanner scanner = new Scanner(new File(filename));
+      while (scanner.hasNextInt()) {
+        for (int i = 0; i < sudoku.matrix.length; i++) {
+          for (int j = 0; j < sudoku.matrix.length; j++) {
+            sudoku.matrix[i][j] = scanner.nextInt();
+          }
+        }
+      }
+      scanner.close();
+      System.out.println("Successful read from " + filename + ".");
+    }
+    // checks if file does not exist
+    catch (FileNotFoundException e) {
+      System.out.println("File not found. I'll quit now.");
+      System.exit(1);
+    }
+    // checks if an error occurred
+    catch (Exception e) {
+      System.out.println("Something went wrong. I'll quit now.");
+      System.exit(1);
+    }
+    return sudoku;
+  }
 }
--- src/Driver.java
+++ src/Driver.java
@@ -1,15 +1,13 @@
 public class Driver {
-    public static void main(String args[]) {
-        Board generator = new Board();
-        String filename = "../boards/" + args[0];
-        generator.generate(filename);
-        Sudoku p1 = generator.readTxt(filename);
-        System.out.println("Sudoku Board:");
-        System.out.println(p1.toString());
-        System.out.println("Solved Sudoku Board:");
-        if (Backtrack.solveSudoku(p1))
-            System.out.println(p1.toString());
-        else
-            System.out.println("No solution.");
-    }
+  public static void main(String args[]) {
+    Board generator = new Board();
+    String filename = "../boards/" + args[0];
+    generator.generate(filename);
+    Sudoku p1 = generator.readTxt(filename);
+    System.out.println("Sudoku Board:");
+    System.out.println(p1.toString());
+    System.out.println("Solved Sudoku Board:");
+    if (Backtrack.solveSudoku(p1)) System.out.println(p1.toString());
+    else System.out.println("No solution.");
+  }
 }
