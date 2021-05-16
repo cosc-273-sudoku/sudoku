f6617cf20a3a857eda93520d234ef4bf71930552
cbw975 <cwohlgemuth22@amherst.edu>
2021-05-16 19:28:43 -0400

    LoneRanger attempt, did not verify

--- src/LoneRangersTask.java
+++ src/LoneRangersTask.java
@@ -1,4 +1,9 @@
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Collections;
 
 public class LoneRangersTask implements Runnable {
   private Board board;
@@ -6,6 +11,7 @@ public class LoneRangersTask implements Runnable {
   private int cornerCol;
   private boolean checkRows;
   private boolean checkCols;
+  private boolean checkMiniGrid;
   private AtomicBoolean valueSet;
 
   public LoneRangersTask(
@@ -26,14 +32,52 @@ public class LoneRangersTask implements Runnable {
 
   public void run() {
     Cell[][] grid = this.board.getGrid();
-    if (checkRows) {
-      for (int r = 0; r < 3; r++) {
-        int row = this.cornerRow + r;
-        if (!this.board.tryLockCell(row, 0)) {
-          continue;
+    Set<Integer> possibleValues = new HashSet<Integer>();
+    HashMap<Integer,List<Integer>> valFreq = new HashMap<Integer, List<Integer>>(); //record for each 1-9 value, the locations it can be in as two values --> row col
+    for(int i=0; i<9; i++) valFreq.put(i,Collections.emptyList());
+
+    int row = 0; int col = 0;
+
+    for (int i = 0; i < 3; i++) {
+      if(checkRows || checkMiniGrid){ row = this.cornerRow + i;
+      }else if(checkCols){ col = this.cornerCol + i;
+      }
+      
+      // if (!this.board.tryLockCell(row, 0)) {
+      //   continue;
+      // }
+
+      for (int j = 0; j < 9; j++) { //for each cell, incrememnt freq counter for values' possible placements
+        if(checkRows){ col = this.cornerCol + j; 
+        }else if(checkCols){ row = this.cornerRow + j;
+        }else{ //checkMiniGrid
+          col = this.cornerCol + j/3;
         }
-        for (int col = 0; col < 9; col++) {}
+        
+        possibleValues = grid[row][col].getPossibleValues();
+        for(Integer num : possibleValues){
+          List<Integer> newLocations = valFreq.get(num); 
+          newLocations.add(row);
+          newLocations.add(col);
+          valFreq.put(num, newLocations); //add the row and col location number can be in to freq counter
+        }
       }
     }
+    
+    //if any values can only be in one place
+    for (HashMap.Entry<Integer,List<Integer>> entry : valFreq.entrySet()){
+      int num = entry.getKey();
+      List<Integer> locations = entry.getValue();
+
+      if(locations.size() == 2){ //if only one location (row and col)
+        row = locations.get(0);
+        col = locations.get(1);
+        grid[row][col].setValue(num); // set value of cell to the single possible value
+        this.valueSet.set(true); // indicate a value has been set in the grid
+        // this.board.removePossibleValue(row, col, value); //remove value from possible set for each cell in row, col, and minigrid
+      }
+
+    }
+
   }
 }
